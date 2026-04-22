00c1ebca021addcc3621728b5c931ec33967296b
Matheus Osorio <henrique.busn@gmail.com>
2026-04-21 23:35:10 -0400

    readme atualizado, variáveis finalizadas

---
+++ sintaxe.py
@@ -0,0 +1,18 @@
+#barra invertida para fazer quebra de linha no codigo, sem precisar usar parenteses
+nome = "Matheus " + \
+    "Osorio"
+
+print (nome)
+
+"""
+Importante  a identação estar certa, muitas problemas começam com erros de identação
+então é bom estar atento a esse tipo de erro, principalmente para quem esta começando
+a programar, pois é um erro comum
+"""
+idade = 18
+
+if idade < 18:
+    print("Menor de idade")
+else:
+    print("Maior de idade")
+
---
+++ variaveis.py
@@ -0,0 +1,17 @@
+#para declaração de variáveis não pode ser usado acentos, espaços ou caracteres especiais
+nome_completo = "Maria da Silva"
+caseSensitive = "Python diferencia maiúsculas de minúsculas"
+snake_case = "variável escrita com palavras separadas por underline"
+camelCase = "variável escrita com palavras juntas, onde cada palavra começa com letra maiúscula, exceto a primeira"
+numero_inteiro = 10
+numero_decimal = 3.14
+"""
+Quando usar camelcase e quando usar snake_case?
+Em Python, a convenção é usar snake_case para variáveis e funções, e camelCase para classes
+Leia o pip8 pra ter mais detalhes sobre as convenções de código em Python
+"""
+
+
+#se tiver dúvida quanto a tipo de dado, use a função type() para verificar o tipo da variável
+print("Tipo da variável numero_inteiro: ", type(numero_inteiro))
+print("Tipo da variável numero_decimal: ", type(numero_decimal))
